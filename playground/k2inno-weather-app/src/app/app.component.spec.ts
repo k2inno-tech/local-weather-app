698e8a9a5edb2dbf518a918bc69c8ad7959bebd1
kk_loke86 <kk86bioinfo@gmail.com>
2020-07-29 10:37:25 +0000

    Revise test plan.

--- playground/k2inno-weather-app/src/app/app.component.spec.ts
+++ playground/k2inno-weather-app/src/app/app.component.spec.ts
@@ -16,16 +16,19 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
+  /*
   it(`should have as title 'k2inno-weather-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('k2inno-weather-app');
   });
+  */
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('k2inno-weather-app app is running!');
+    expect(compiled.querySelector('h1').textContent)
+      .toContain('K2innoCast Weather')
   });
 });
